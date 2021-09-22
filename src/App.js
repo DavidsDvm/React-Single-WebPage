df6c0d30466deb61c0e52195150ac25644be3473
DavidsDvm <davids10.dvm@gmail.com>
2021-09-22 16:44:20 -0500

    new changes and callout of the BD API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Charactes from './components/Characters';
 
 function App() {
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = 'https://breakingbadapi.com/api/characters';
+    axios.get(apiUrl).then((repos) => {
+      const allRepos = repos.data;
+      setAppState({ loading: false, repos: allRepos });
+    });
+  }, [setAppState]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+      <Header />
+      <Charactes isLoading={appState.loading} repos={appState.repos}/>
+      <Footer />
+    </>
   );
 }
 
