746d0d69ae798ed25a8f4cdc1e54815e4ea6694d
DavidsDvm <davids10.dvm@gmail.com>
2021-09-22 16:46:26 -0500

    Footer component of the project

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = (props) => {
+    return (
+        <footer className="footer mt-auto text-white py-3 bg-dark">
+            <p class="footer-heart">
+                Made with <g-emoji class="g-emoji" alias="heart" fallback-src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png">
+                <img class="emoji" alt="heart" height="20" width="20" src="https://github.githubassets.com/images/icons/emoji/unicode/2764.png" /></g-emoji> by <a href="https://github.com/DavidsDvm">DavidsDvm</a>
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
