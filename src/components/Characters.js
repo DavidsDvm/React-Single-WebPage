b45d489d26460252bbfe8551121bbb8eda266476
DavidsDvm <davids10.dvm@gmail.com>
2021-09-24 22:05:29 -0500

    creation fo the module characters with all characters data and new individual character view with sweet alert

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,35 +1,61 @@
-import { useEffect, useState } from 'react';
 import React from 'react';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
+import Button from './Button';
 
 const Characters = (props) => {
     const { repos } = props;
+    const MySwal = withReactContent(Swal)
 
     if (!repos || repos.length === 0) return <p>No repos, sorry</p>;
+
+    const btnView = (id) => {
+        repos.forEach((repo) => {
+            if (repo.char_id == id) {
+                MySwal.fire({
+                    title: '<br />' + repo.name,
+                    html: '<b>Fecha de cumpleanos: </b>' + repo.birthday + '<br>' +
+                            '<b>Apodo: </b>' + repo.nickname + '<br>' +
+                            '<b>Ocupacion: </b>' + repo.occupation + '<br>' +
+                            '<b>Estado: </b>' + repo.status + '<br>' +
+                            '<b>Temporadas en las que aparece: </b>' + repo.appearance + '<br>' +
+                            '<b>Interpretado por: </b>' + repo.portrayed + '<br>' +
+                            '<b>Series en las que aparece: </b>' + repo.category,
+                    imageUrl: repo.img,
+                    imageWidth: '70%',
+                    imageHeight: '100%',
+                    imageAlt: 'Image of: ' + repo.name,
+                })
+            }
+        })
+        
+    }
 
     return (
         <div className="bg-dark text-white">
             <div className="container">
                 <h2 className='list-head'>BD personajes</h2>
-                <div class="row row-cols-1 row-cols-sm-2 g-2">
+                <div className="row row-cols-1 row-cols-sm-2 g-2">
                     {repos.map((repo) => {
                         return (
-                            <div key={repo.id} class="col">
-                                <div style={{margin: '1% 5% 0 1%'}} class="card shadow-sm card-colors">
-                                    <div class="card-header text-center">
+                            <div key={repo.char_id} className="col">
+                                <div style={{margin: '1% 5% 0 1%'}} className="card shadow-sm card-colors">
+                                    <div className="card-header text-center">
                                         <h1>{repo.name}</h1>
                                     </div>
 
-                                    <img style={{margin: 'auto'}} src={repo.img} alt="asd" width="65%" height="225" />
+                                    <img style={{margin: 'auto'}} src={repo.img} alt="asd" width="30%" height="30%" onClick={() => btnView(repo.char_id)} onMouseEnter={(e) => {e.target.style.cursor = 'pointer'}}/>
             
-                                    <div class="card-body">
-                                        <p class="card-text"><b>Ocupacion:</b> {repo.occupation}</p>
-                                        <p class="card-text"><b>Fecha de cumpleanos:</b> {repo.birthday}</p>
-                                        <p class="card-text"><b>Estado:</b> {repo.status}</p>
-                                        <p class="card-text"><b>Apodo:</b> {repo.nickname}</p>
-                                        <div class="d-flex flex-row-reverse">
-                                            <div style={{marginLeft: 'auto'}} class="btn-group align-items-right">
-                                                <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                                                <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                    <div className="card-body">
+                                        <p className="card-text"><b>Ocupacion:</b> {repo.occupation}</p>
+                                        <p className="card-text"><b>Fecha de cumpleanos:</b> {repo.birthday}</p>
+                                        <p className="card-text"><b>Estado:</b> {repo.status}</p>
+                                        <p className="card-text"><b>Apodo:</b> {repo.nickname}</p>
+                                        <div className="d-flex flex-row-reverse">
+                                            <div style={{marginLeft: 'auto'}} className="btn-group align-items-right">
+                                                <Button kindClasses='btn btn-sm btn-outline-secondary' text='view' onClick={() => btnView(repo.char_id)}/>
+                                                <Button kindClasses='btn btn-sm btn-outline-secondary' text='edit'/>
                                             </div>
                                         </div>
                                     </div>
